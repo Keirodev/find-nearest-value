d8ffcf01919619173aca63bd0ac863a088f1482c
Kévin Monteiro <keiro.dev@gmail.com>
2022-05-07 01:27:05 +0200

    Fully working version. Todo : copy/paste button

--- order.js
+++ order.js
@@ -1,3 +1,15 @@
+/*
+
+Testing Data
+
+2332-Spike
+232-Nico
+190910-Brahms34
+140910-Pac
+
+ */
+
+
 document.getElementById('submit').addEventListener('click', event => {
   // avoid native submit page reload
   event.preventDefault()
@@ -14,7 +26,43 @@ document.getElementById('submit').addEventListener('click', event => {
   }
 
   // extract values as 0000-xxxx
+  const splittedValue = values.split(/([0-9]{1,10}-\w+)/gm)
+  const cleanedValue = splittedValue
+    .filter(value => /([0-9]{1,10}-\w+)/g.test(value))
+    .map(value => {
+      let score, name;
+      [score, name] = value.split('-')
+      return {score: Number(score), name}
+    })
 
+  // now let's order
+  cleanedValue.sort((a, b) => Math.abs(target - Number(a.score)) - Math.abs(target - Number(b.score)))
+  // add ≠ to target
+  const results = cleanedValue.map(item => {
+    return {
+      ...item,
+      delta: Math.abs(item.score - target)
+    }
+  })
+
+  // add results to dom
+  const domResults = results.map((result, index) => `
+        <div class="row">
+          <div class="col-3">${index + 1}</div>
+          <div class="col-3">${result.name}</div>
+          <div class="col-3">${result.score}</div>
+          <div class="col-3">${result.delta}</div>
+        </div>
+    `)
+
+  domResults.unshift(`<div class="row">
+          <div class="col-3">Position</div>
+          <div class="col-3">Nom</div>
+          <div class="col-3">Estimation</div>
+          <div class="col-3">Différence</div>
+        </div>`)
+
+  document.getElementById('results').innerHTML = domResults.join('');
 
 })
 
@@ -34,3 +82,6 @@ function toggleDisplayError(message) {
 
 
 }
+
+
+// TODO : btm poun copier  / coller ler résultats finaux.
